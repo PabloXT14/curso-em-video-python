69eba0cbbe8148c2a2975bcf77807776ae38b853
Pablo Alan <pabloxt14@gmail.com>
2023-10-29 18:11:46 -0300

    docs: done ex002

---
+++ execicios/ex002.py
@@ -0,0 +1,11 @@
+# Desafio 02
+
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+# Resolução
+
+user_name = input('Digite seu nome: ')
+
+# print('É um prazer te conhecer, ', user_name, '!')
+# print('É um prazer te conhecer, {}!'.format(user_name))
+print(f'É um prazer te conhecer, {user_name}!')
