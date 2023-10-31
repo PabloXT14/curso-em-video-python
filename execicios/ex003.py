11be4d3ca77d02ac60a11823ea926095b909f5fe
Pablo Alan <pabloxt14@gmail.com>
2023-10-31 20:07:24 -0300

    docs: done exercise 004

---
+++ execicios/ex003.py
@@ -0,0 +1,12 @@
+# Desafio 03
+
+# Crie um programa que leia dois número e mostre a soma entre eles.
+
+# Resolução
+
+n1 = int(input('Digite o 1º número: '))
+n2 = int(input('Digite o 2º número: '))
+
+result = n1 + n2
+
+print(f'Resultado: {n1} + {n2} = {result}')
