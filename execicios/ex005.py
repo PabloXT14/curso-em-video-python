a64793fdddbb7bb2ffa43600ccccee06786e7616
Pablo Alan <pabloxt14@gmail.com>
2023-11-02 22:55:35 -0300

    docs: done ex005

---
+++ execicios/ex005.py
@@ -0,0 +1,11 @@
+# Exercício 5
+
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor. 
+
+# Resolução
+
+number = int(input('Digite um número inteiro: '))
+
+print(f'Antecessor: {number - 1}')
+print(f'Sucessor: {number + 1}')
+print(f'{number - 1} < {number} < {number + 1}')
