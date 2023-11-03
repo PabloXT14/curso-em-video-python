a747868725cf73391ca4b92ff26970591097c8eb
Pablo Alan <pabloxt14@gmail.com>
2023-11-02 23:00:31 -0300

    docs: done ex006

---
+++ execicios/ex006.py
@@ -0,0 +1,13 @@
+# # Exercício 5
+
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada. 
+
+# Resolução
+
+print('Saiba o dobre, triplo e raiz quadrada de um número')
+
+number = int(input('Digite um número: '))
+
+print(f'Dobro de {number} == {number * 2}')
+print(f'Triplo de {number} == {number * 3}')
+print(f'Raiz quadrada de {number} == {number**(1/2)}')
