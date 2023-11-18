d0ee3ea3df1fd78fb815188a286051352c2bde62
Pablo Alan <pabloxt14@gmail.com>
2023-11-17 21:52:22 -0300

    refactor: ex016

--- execicios/ex016.py
+++ execicios/ex016.py
@@ -10,9 +10,29 @@ print('Quebrando um número')
 
 number = float(input('Digite um número inteiro qualquer: '))
 
+# 1ª Solução
+
 decimal_part, integer_part = math.modf(number) 
 
 print('-' * 20)
-print(f'Parte inteira: {integer_part}')
+print(f'Parte inteira: {int(integer_part)}')
 print(f'Parte decimal: {round(decimal_part, 2)}')
 print('-' * 20)
+
+# 2ª Solução
+'''
+integer_part = math.trunc(number) # a função trunc (de truncate) retorna a parte inteira de um número real 
+
+print('-' * 20)
+print(f'Parte inteira: {integer_part}')
+print('-' * 20)
+'''
+
+# 3ª Solução
+'''
+integer_part = int(number)
+
+print('-' * 20)
+print(f'Parte inteira: {integer_part}')
+print('-' * 20)
+'''
