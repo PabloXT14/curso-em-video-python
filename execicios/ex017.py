eb903b80f635008424abef88055adc7673d02596
Pablo Alan <pabloxt14@gmail.com>
2023-11-18 22:07:12 -0300

    refactor: ex017

--- execicios/ex017.py
+++ execicios/ex017.py
@@ -7,12 +7,31 @@ import math
 
 print('Catetos e Hipotenusa')
 
+print('-' * 40)
+
 cat_adj = float(input('Digite o valor do cateto adjacente: '))
 cat_opo = float(input('Digite o valor do cateto oposto: '))
 
 # Fórmula: hip² = cat² + cat² 
+
+# 1ª Solução
+hip = math.hypot(cat_adj, cat_opo)
+
+print('-' * 40)
+print(f'Hipotenusa: {hip:.2f}')
+
+# 2ª Solução
+'''
 hip = math.sqrt(math.pow(cat_adj, 2) + math.pow(cat_opo, 2))
 
-print('-' * 20)
-print(f'Hipotenusa: {hip}')
-print('-' * 20)
+print('-' * 40)
+print(f'Hipotenusa: {hip:.2f}')
+'''
+
+# 3ª Solução
+'''
+hip = (cat_opo ** 2 + cat_adj ** 2) ** (1/2)
+
+print('-' * 40)
+print(f'Hipotenusa: {hip:.2f}')
+'''
