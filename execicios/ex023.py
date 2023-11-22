ef5d749ceecb0dff7d74d20c46c72b3d3f958601
Pablo Alan <pabloxt14@gmail.com>
2023-11-22 17:15:25 -0300

    docs: done ex023

---
+++ execicios/ex023.py
@@ -0,0 +1,42 @@
+# Exercício 23 - Separando digitos de um número (Aula 009)
+
+'''
+# Descrição: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+Ex:
+Digite um número: 1834
+
+- Unidade: 4
+- Dezena: 3
+- Centena: 8
+- Milhar: 1
+'''
+
+# Resolução:
+
+print('Separando digitos de um número')
+
+print('-' * 80)
+
+while True:
+    try:
+        number = int(input('Digite um número (de 0 a 9999): '))
+
+        if (number >= 0 and number <= 9999):
+            break
+    except:
+        print('Entrada inválida.')
+    
+print('-' * 80)
+
+str_number = str(number)
+number_digits_amount = len(str_number)
+
+unit = str_number[number_digits_amount - 1]
+ten = str_number[number_digits_amount - 2] if (number_digits_amount >= 2) else 'sem dezenas'
+hundred = str_number[number_digits_amount - 3] if (number_digits_amount >= 3) else 'sem centenas'
+thousands = str_number[number_digits_amount - 4] if (number_digits_amount == 4) else 'sem milhar'
+
+print(f'- Unidade: {unit}')
+print(f'- Dezena: {ten}')
+print(f'- Centena: {hundred}')
+print(f'- Milhar: {thousands}')
