74e16b623e7aa3c5337f1c4e204a6a52183abdc7
Pablo Alan <pabloxt14@gmail.com>
2023-11-25 17:35:11 -0300

    refactor: ex026

--- execicios/ex026.py
+++ execicios/ex026.py
@@ -13,17 +13,16 @@ print('Primeira e última ocorrência de uma string')
 
 print('-' * 80)
 
-phrase = input('Digite uma frase: ')
+phrase = str(input('Digite uma frase: ')).strip().lower()
 
 print('-' * 80)
-
-phrase = phrase.lower()
 
 count_a_letter = phrase.count('a')
 
 if (count_a_letter > 0):
-    first_appearance_letter_a = phrase.find('a')
-    last_appearance_letter_a = phrase.rfind('a')
+    # somamos um pois as posições começam em 0
+    first_appearance_letter_a = phrase.find('a') + 1 
+    last_appearance_letter_a = phrase.rfind('a') + 1
 
     print(f"Quantidade de vezes que aparece a letra 'A': {count_a_letter}")
     print(f'Posição em que ela aparece a primeira vez: {first_appearance_letter_a}')
