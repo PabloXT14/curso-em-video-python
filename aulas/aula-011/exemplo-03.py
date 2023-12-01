7fdb289bd38e0d21ef115ae14d29a5344f46d8b0
Pablo Alan <pabloxt14@gmail.com>
2023-12-01 17:47:54 -0300

    docs: add more details in class 011 content

--- aulas/aula-011/exemplo-03.py
+++ aulas/aula-011/exemplo-03.py
@@ -8,11 +8,33 @@ print(f'Olá, muito prazer em te conhecer {'\033[4;44m'}{name}{'\033[m'}')
 
 print('-' * 70)
 
-colors = {
+styles = {
     'reset': '\033[m',
-    'blue': '\033[34m',
-    'yellow': '\033[33m',
-    'negative': '\033[7;37m'
+    'bold': '\033[1m',
+    'underline': '\033[4m',
+    'reverse': '\033[7m'
 }
 
-print(f'Olá, muito prazer em te conhecer {colors["negative"]}{name}{colors["reset"]}')
+colors = {
+    'black': "\033[30m",
+    'red': "\033[31m",
+    'green': "\033[32m",
+    'yellow': "\033[33m",
+    'blue': "\033[34m",
+    'magenta': "\033[35m",
+    'cyan': "\033[36m",
+    'white': "\033[37m"
+}
+
+backgrounds = {
+    'black': '\033[40m',
+    'red': '\033[41m',
+    'green': '\033[42m',
+    'yellow': '\033[43m',
+    'blue': '\033[44m',
+    'magenta': '\033[45m',
+    'cyan': '\033[46m',
+    'white': '\033[47m'
+}
+
+print(f'Olá, muito prazer em te conhecer {styles['reverse']}{colors['white']}{backgrounds['red']}{name}{styles['reset']}')
