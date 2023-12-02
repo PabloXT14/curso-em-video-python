7604ff4be84e5a841f37b1a2f03eed6ccc571b8c
Pablo Alan <pabloxt14@gmail.com>
2023-12-02 18:11:48 -0300

    refactor: add colors ex033

--- execicios/ex033.py
+++ execicios/ex033.py
@@ -8,6 +8,37 @@ Faça um programa que leia três números e mostre qual é o 'maior' e qual é o
 
 # Resolução:
 
+styles = {
+    'reset': '\033[m',
+    'bold': '\033[1m',
+    'underline': '\033[4m',
+    'reverse': '\033[7m'
+}
+
+colors = {
+    'black': '\033[30m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'magenta': '\033[35m',
+    'cyan': '\033[36m',
+    'gray': '\033[37m',
+    'white': '\033[97m'
+}
+
+backgrounds = {
+    'black': '\033[40m',
+    'red': '\033[41m',
+    'green': '\033[42m',
+    'yellow': '\033[43m',
+    'blue': '\033[44m',
+    'magenta': '\033[45m',
+    'cyan': '\033[46m',
+    'gray': '\033[47m',
+    'white': '\033[107m'
+}
+
 print('# Maior e menor valores')
 
 print('-' * 70)
@@ -57,5 +88,5 @@ elif (number_3 < lowest_number):
 
 print('-' * 70)
 
-print(f'* O maior número é: {highest_number}')
-print(f'* O menor número é: {lowest_number}')
+print(f'* O maior número é: {styles["bold"] + colors["green"]}{highest_number}{styles["reset"]}')
+print(f'* O menor número é: {styles["bold"] + colors["red"]}{lowest_number}{styles["reset"]}')
