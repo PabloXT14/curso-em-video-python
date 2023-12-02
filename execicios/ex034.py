3a87e490f70794e196443b8dc2dd46f69837feae
Pablo Alan <pabloxt14@gmail.com>
2023-12-02 18:18:05 -0300

    refactor: add colors ex034

--- execicios/ex034.py
+++ execicios/ex034.py
@@ -12,6 +12,37 @@ Para inferiores ou iguais, o aumento é de 15%.
 
 # Resolução:
 
+styles = {
+    'reset': '\033[m',
+    'bold': '\033[1m',
+    'underline': '\033[4m',
+    'reverse': '\033[7m'
+}
+
+colors = {
+    'black': '\033[30m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'magenta': '\033[35m',
+    'cyan': '\033[36m',
+    'gray': '\033[37m',
+    'white': '\033[97m'
+}
+
+backgrounds = {
+    'black': '\033[40m',
+    'red': '\033[41m',
+    'green': '\033[42m',
+    'yellow': '\033[43m',
+    'blue': '\033[44m',
+    'magenta': '\033[45m',
+    'cyan': '\033[46m',
+    'gray': '\033[47m',
+    'white': '\033[107m',
+}
+
 print('Aumentos multiplos')
 
 print('-' * 70)
@@ -20,10 +51,10 @@ wage = float(input('Digite seu salário (R$): '))
 
 print('-' * 70)
 
-primary_salary_increase = 10/100
-secondary_salary_increase = 15/100
+primary_salary_increase = 15/100
+secondary_salary_increase = 10/100
 
-fits_the_primary_increase = wage > 1250
+fits_the_primary_increase = wage <= 1250
 
 new_wage = 0
 increase_value = 0
@@ -35,6 +66,6 @@ else:
     increase_value = wage * secondary_salary_increase
     new_wage = wage + increase_value
 
-print(f'* Antigo salário: R$ {wage:.2f}')
-print(f'* Valor de aumento: R$ {increase_value:.2f}')
-print(f'* Novo salário: R$ {new_wage:.2f}')
+print(f'* Antigo salário: {styles["bold"] + colors["yellow"]}R$ {wage:.2f}{styles["reset"]}')
+print(f'* Valor de aumento: {styles["bold"] + colors["yellow"]}R$ {increase_value:.2f}{styles["reset"]}')
+print(f'* Novo salário: {styles["bold"] + colors["yellow"]}R$ {new_wage:.2f}{styles["reset"]}')
