5ebd64aff4640d8b8fee3e13ce1d0606b2742468
Pablo Alan <pabloxt14@gmail.com>
2023-12-06 14:27:30 -0300

    refactor: ex037

--- execicios/ex037.py
+++ execicios/ex037.py
@@ -12,6 +12,18 @@ Escreva um programa que leia um número inteiro qualquer e peça para o usuário
 
 # Resolução:
 
+'''
+# Explicando Conceitos
+O [2:] é uma notação de fatiamento (slicing) de texto (string) em Python. Quando você converte um número para binário, octal ou hexadecimal usando as funções embutidas bin, oct ou hex, o resultado é uma string que inclui um prefixo indicando a base.
+
+Por exemplo:
+
+Binário: 0b (prefixo)
+Octal: 0o (prefixo)
+Hexadecimal: 0x (prefixo)
+O [2:] é usado para remover esse prefixo e obter apenas a parte significativa do número. Por exemplo, se o número binário gerado for 0b1101, usar [2:] resultará em 1101, que é a representação binária sem o prefixo 0b
+'''
+
 print('Conversor de Bases Numéricas')
 
 print('-' * 70)
@@ -32,16 +44,20 @@ while True:
 
     print('-' * 70)
 
-    if (option == 1) or (option == 2) or (option == 3):
+    if 1 <= int(option) <= 3:
         break
+    else:
+        print('Escolha inválida. Por favor digite uma das opções disponíveis.')
+    
+    print('-' * 70)
 
 
 if (option == 1):
-    binary_value = bin(number)
-    print(f'* Valor em Binário: {binary_value}')
+    binary_value = bin(number)[2:]
+    print(f'* Valor em Binário: {'\033[1;33m'}{binary_value}{'\033[m'}')
 elif (option == 2):
-    octal_value = oct(number)
-    print(f'* Valor em Octal: {octal_value}')
+    octal_value = oct(number)[2:]
+    print(f'* Valor em Octal: {'\033[1;33m'}{octal_value}{'\033[m'}')
 else:
-    hexadecimal_value = hex(number)
-    print(f'* Valor em Hexadecimal: {hexadecimal_value}')
+    hexadecimal_value = hex(number)[2:]
+    print(f'* Valor em Hexadecimal: {'\033[1;33m'}{hexadecimal_value}{'\033[m'}')
