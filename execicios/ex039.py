1d17959338265a07be1906cd3f0609a341aa087e
Pablo Alan <pabloxt14@gmail.com>
2023-12-06 15:11:03 -0300

    refactor: ex039

--- execicios/ex039.py
+++ execicios/ex039.py
@@ -17,12 +17,36 @@ Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 # Resolução:
 
 from datetime import datetime
+from sys import exit
 
 current_year = datetime.now().year
 
 print('# Alistamento Militar')
 
 print('-' * 70)
+
+gender = ''
+
+while True:
+    print('Qual o seu sexo:')
+    print('H - Homem')
+    print('M - Mulher')
+
+    gender = str(input('Digite a letra correspondente ao seu sexo: ')).lower()
+
+    print('-' * 70)
+
+    if (gender == 'h' or gender == 'm'):
+        break
+    else:
+        print('Escolha inválida. Por favor digite uma das opções disponíveis.')
+
+    print('-' * 70)
+
+if (gender == 'm'):
+    print(f'{'\033[1;33m'}* Você não precisa fazer o alistamento militar obrigatório.{'\033[m'}')
+    exit()
+
 
 birth_year = int(input('Digite o seu ano de nascimento: '))
 
@@ -37,11 +61,16 @@ if (candidate_age < minimum_age_enlistment):
     missing_years =  minimum_age_enlistment - candidate_age
 
     print('* Você ainda vai se alistar ao serviço militar.')
-    print(f'* Faltam {'\033[1;33m'}{missing_years} ano(s){'\033[m'}.')
+    print(f'* Falta(m) {'\033[1;33m'}{missing_years} ano(s){'\033[m'}.')
+    print(f'* Seu alistamento será no ano: {'\033[1;33m'}{current_year + missing_years}{'\033[m'}')
+
 elif (candidate_age == minimum_age_enlistment):
     print(f'{'\033[1;33m'}* É a hora de se alistar ao serviço militar.{'\033[m'}')
+    print(f'{'\033[1;33m'}* Vá a uma junta militar o mais breve possível.{'\033[m'}')
+
 else:
     past_years = candidate_age - minimum_age_enlistment
 
     print('* Já passou do tempo do alistamento.')
     print(f'* Passaram {'\033[1;33m'}{past_years} ano(s){'\033[m'}.')
+    print(f'* Seu alistamento foi no ano: {'\033[1;33m'}{current_year - past_years}{'\033[m'}')
