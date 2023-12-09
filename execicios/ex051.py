c90e68a47c1aaf3a5b405f7e04052adedb6ac5f7
Pablo Alan <pabloxt14@gmail.com>
2023-12-09 13:21:57 -0300

    docs: done ex051

---
+++ execicios/ex051.py
@@ -0,0 +1,31 @@
+# Exercício 51 - Progressão Aritmética (013)
+
+'''
+# Descrição:
+
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+'''
+
+# Resolução:
+
+# Fórmula PA: An = A1 + (n - 1) * r
+
+print('# Progressão Aritmética')
+
+print('-' * 70)
+
+first_term = int(input('Digite o primeiro termo da PA: '))
+ratio = int(input('Digite a razão da PA: '))
+
+print('-' * 70)
+
+print('* 10 primeiros termos da PA:')
+
+for i in range(1, 11):
+    current_term = first_term + (i - 1) * ratio
+
+    if (i % 10 != 0):
+        print(current_term, end=' - ')
+    else:
+        print(current_term)
+        
