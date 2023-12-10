a63a75fb45f2b48f0886a0ca681f7107d0a06ec8
Pablo Alan <pabloxt14@gmail.com>
2023-12-10 17:14:34 -0300

    done: ex055

---
+++ execicios/ex055.py
@@ -0,0 +1,32 @@
+# Exercício 55 - Maior e menor da sequência  (013)
+
+'''
+# Descrição:
+
+Faça um programa que leia o peso de 5 pessoas. No final, mostre qual foi o maior e menor peso lidos.
+'''
+
+# Resolução:
+
+print('# Maior e menor da sequência')
+
+people_amount = 5
+lower_weight = float('inf') # inicializa com o maior valor possível (inf = infinito positivo)
+higher_weight = float('-inf') # inicializa com o menor valor possível (-int = infinito negativo)
+
+print('-' * 70)
+
+for i in range(0, people_amount):
+    person_weight = float(input(f'Digite o peso da pessoa {i + 1} (kg): '))
+
+    if (person_weight > higher_weight):
+        higher_weight = person_weight
+    
+    if (person_weight < lower_weight):
+        lower_weight = person_weight
+    
+    print('-' * 70)
+
+print('* Resultados:')
+print(f'* Maior peso lido: {'\033[1;33m'}{higher_weight:.2f} kg{'\033[m'}')
+print(f'* Menor peso lido: {'\033[1;33m'}{lower_weight:.2f} kg{'\033[m'}')
