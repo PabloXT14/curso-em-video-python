e5892b08b447ac9bdca66f5e19724d86161a1212
Pablo Alan <pabloxt14@gmail.com>
2023-12-11 15:56:02 -0300

    refactor: ex051

--- execicios/ex051.py
+++ execicios/ex051.py
@@ -21,6 +21,8 @@ print('-' * 70)
 
 print('* 10 primeiros termos da PA:')
 
+# 1ª Solução
+'''
 for i in range(1, 11):
     current_term = first_term + (i - 1) * ratio
 
@@ -28,4 +30,13 @@ for i in range(1, 11):
         print(current_term, end=' - ')
     else:
         print(current_term)
-        
+'''
+
+# 2ª Solução
+tenth_term = first_term + (10 - 1) * ratio
+
+for i in range(first_term, tenth_term + ratio, ratio):
+    if (i != tenth_term):
+        print(i, end=' - ')
+    else:
+        print(i)
