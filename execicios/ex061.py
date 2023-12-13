2df2a871e88b8938bdd68a913c354d8e713af90d
Pablo Alan <pabloxt14@gmail.com>
2023-12-13 20:10:51 -0300

    done: ex061

---
+++ execicios/ex061.py
@@ -0,0 +1,33 @@
+# Exercício 61 - Progressão Aritmética v2.0 (014)
+
+'''
+# Descrição:
+
+Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+'''
+
+# Resolução:
+
+# Fórmula PA: An = A1 + (n - 1) * r
+
+print('# Progressão Aritmética')
+
+print('-' * 70)
+
+first_term = int(input('Digite o primeiro termo da PA: '))
+ratio = int(input('Digite a razão da PA: '))
+
+print('-' * 70)
+
+current_term = first_term
+count = 1
+terms_amount = 10
+
+while count <= terms_amount:
+    if (count < terms_amount):
+        print(f'{current_term}', end=' - ')
+    else:
+        print(f'{current_term}')
+
+    current_term += ratio
+    count += 1
