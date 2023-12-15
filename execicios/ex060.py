98b87b2ec56287b5bbc30b432a518c1c5d8c8c13
Pablo Alan <pabloxt14@gmail.com>
2023-12-15 17:22:38 -0300

    refactor: ex060

--- execicios/ex060.py
+++ execicios/ex060.py
@@ -15,23 +15,22 @@ print('# Cálculo do Fatorial')
 
 print('-' * 70)
 
-number = int(input('Digite um número: '))
+number = int(input('Digite um número para calcular seu fatorial: '))
 
 print('-' * 70)
 
+# 1ª Solução
 if (number >= 0):
 
     factorial_result = 1
 
-    can_continue = True if (number > 0) else False
+    can_continue = True if (number > 1) else False
 
     print(f'{number}! = ', end='')
 
     while can_continue:
-        if (number != 1):
-            print(f'{number} x ', end='')
-        else:
-            print(f'{number} = ', end='')
+        print(f'{number}', end='')
+        print(' x ' if number > 1 else ' = ', end='')
 
         factorial_result *= number
 
@@ -44,3 +43,36 @@ if (number >= 0):
 
 else:
     print(f'{'\033[1;33m'}* Por favor, digite um número não negativo.{'\033[m'}')
+    
+
+# 2ª Solução
+'''
+if (number >= 0):
+    print(f'{number}! = ', end='')
+
+    factorial_result = 1
+
+    if (number > 1):
+        for i in range(number, 0, -1):
+            factorial_result *= i
+
+            print(f'{i}', end='')
+            print(' x ' if i > 1 else ' = ', end='')
+    
+    print(f'{'\033[1;33m'}{factorial_result}{'\033[m'}')
+else:
+    print(f'{'\033[1;33m'}* Por favor, digite um número não negativo.{'\033[m'}')
+'''
+
+    
+# 3ª Solução
+'''
+from math import factorial
+
+if (number >= 0):
+    factorial_result = factorial(number) 
+
+    print(f'* O fatorial de {number} é: {'\033[1;33m'}{factorial_result}{'\033[m'}')
+else:
+    print(f'{'\033[1;33m'}* Por favor, digite um número não negativo.{'\033[m'}')
+'''
