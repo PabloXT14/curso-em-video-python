792f12b493cacd54fcc5fef2c9409839d64cb9b2
Pablo Alan <pabloxt14@gmail.com>
2023-12-20 13:50:02 -0300

    refactor: ex072

--- execicios/ex072.py
+++ execicios/ex072.py
@@ -12,26 +12,45 @@ Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por
 
 print('# Número por Extenso')
 
-print('-' * 100)
+line_length = 100
+
+print('-' * line_length)
 
 numbers_by_extent = (
     'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
 )
 
-while True:
-    number = int(input('Digite um número inteiro (entre 0 e 20): '))
+can_continue = True
 
-    if 0 <= number <= 20:
-        break
-    else:
-        print('-' * 100)
+while can_continue:
+
+    while True:
+        number = int(input('Digite um número inteiro (entre 0 e 20): '))
+
+        if 0 <= number <= 20:
+            break
+        
+        print('-' * line_length)
 
         print(f'{'\033[1;31m'}Entrada inválida. Por favor, digite um número dentro do intervalo mostrado.{'\033[m'}')
 
-        print('-' * 100)
+        print('-' * line_length)
 
-print('-' * 100)
+    print('-' * line_length)
 
-number_name = numbers_by_extent[number].capitalize()
+    number_name = numbers_by_extent[number].capitalize()
 
-print(f'* Número {number} por extenso: {'\033[1;33m'}{number_name}{'\033[m'}')
+    print(f'* Número {number} por extenso: {'\033[1;33m'}{number_name}{'\033[m'}')
+
+    print('-' * line_length)
+
+    while True:
+        answer = str(input('Deseja digitar outro número? (S/N): ')).strip().upper()
+
+        if answer == 'S':
+            break
+        elif answer == 'N':
+            can_continue = False
+            break
+    
+    print('-' * line_length)
