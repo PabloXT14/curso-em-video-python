5a6e0ced07166595199a5ea1c34574276db2820f
Pablo Alan <pabloxt14@gmail.com>
2023-12-25 00:53:27 -0300

    docs: done ex084

---
+++ execicios/ex084.py
@@ -0,0 +1,71 @@
+# Exercício 84 - Lista Composta e Análise de Dados (018)
+
+'''
+# Descrição:
+
+Faça um programa que leia o nome e peso de várias pessoas, guardando tudo em uma lista. No final, mostre:
+
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma listagem com as pessoas mais leves.
+'''
+
+# Resolução:
+
+line_length = 71
+
+print('# Lista Composta e Análise de Dados')
+
+people = []
+highest_weight = 0
+lowest_weight = 0
+heaviest_people = []
+lighter_people = []
+
+while True:
+    person = []
+    print(f'{'-' * int((line_length - 10) / 2)} {len(people) + 1}ª PESSOA {'-' * int((line_length - 10) / 2)}')
+
+    name = str(input('Nome: '))
+    weight = int(input('Peso (kg): '))
+
+    person = [name, weight]
+
+    if len(people) == 0:
+        highest_weight = weight
+        lowest_weight = weight
+    else:
+        if weight > highest_weight:
+            highest_weight = weight
+        elif weight < lowest_weight:
+            lowest_weight = weight
+
+    people.append(person[:])
+
+    person.clear()
+
+    while True:
+        answer = str(input('Deseja continuar? (S/N): ')).strip().upper()
+
+        if answer in 'SN':
+            break
+    
+    print('-' * line_length)
+    
+    if answer == 'N':
+        break
+
+for person in people:
+     person_name = person[0]
+     person_weight = person[1]
+
+     if person_weight == highest_weight:
+         heaviest_people.append(person_name)
+     
+     if person_weight == lowest_weight:
+         lighter_people.append(person_name)
+
+
+print(f'* Quantidade de pessoas cadastradas: {'\033[1;33m'}{len(people)}{'\033[m'}')
+print(f'* Pessoas mais pesadas ({highest_weight:.2f} kg): {'\033[1;33m'}{heaviest_people}{'\033[m'}')
+print(f'* Pessoas mais leves ({lowest_weight:.2f} kg): {'\033[1;33m'}{lighter_people}{'\033[m'}')
