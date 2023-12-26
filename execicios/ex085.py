caaa851e141d2a56eca69ff9415f0960b349ca15
Pablo Alan <pabloxt14@gmail.com>
2023-12-26 18:51:45 -0300

    docs: done ex085

---
+++ execicios/ex085.py
@@ -0,0 +1,36 @@
+# Exercício 85 - Lista com Pares e Ímpares (018)
+
+'''
+# Descrição:
+
+Crie um programa onde o usuário possa digitar 7 valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
+'''
+
+# Resolução:
+
+line_length = 70
+
+print('# Lista com Pares e Ímpares')
+
+print('-' * line_length)
+
+numbers = [[], []]
+
+for i in range(0, 7):
+    number = int(input(f'Digite o {i + 1}º valor: '))
+
+    if number % 2 == 0:
+        numbers[0].append(number)
+    else:
+        numbers[1].append(number)
+
+print('-' * line_length)
+
+even_numbers = numbers[0][:]
+odd_numbers = numbers[1][:]
+
+even_numbers.sort()
+odd_numbers.sort()
+
+print(f'* Números pares: {'\033[1;33m'}{even_numbers}{'\033[m'}')
+print(f'* Números ímpares: {'\033[1;33m'}{odd_numbers}{'\033[m'}')
