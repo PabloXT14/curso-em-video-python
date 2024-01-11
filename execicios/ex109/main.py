276f56a487afeb9083a6704cf7f3da98d661157c
Pablo Alan <pabloxt14@gmail.com>
2024-01-11 18:21:09 -0300

    refactor: ex109

--- execicios/ex109/main.py
+++ execicios/ex109/main.py
@@ -15,6 +15,6 @@ print('-' * line_length)
 print(f'* Metade de {currency(price)}: {'\033[1;33m'}{half(price, price_format=True)}{'\033[m'}')
 print(f'* Dobro de {currency(price)}: {'\033[1;33m'}{double(price, price_format=True)}{'\033[m'}')
 print(f'* Aumentando {increase_percentage}% (em {currency(price)}) temos: {'\033[1;33m'}{increase(price, increase_percentage, price_format=True)}{'\033[m'}')
-print(f'* Diminuindo {decrease_percentage}% (em {currency(price)}) temos: {'\033[1;33m'}{decrease(price, increase_percentage, price_format=True)}{'\033[m'}')
+print(f'* Diminuindo {decrease_percentage}% (em {currency(price)}) temos: {'\033[1;33m'}{decrease(price, decrease_percentage, price_format=True)}{'\033[m'}')
 
 print('-' * line_length)
