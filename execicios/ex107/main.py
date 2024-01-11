8b6d60ca5c39d12a546b914ea00a52d021f41d2b
Pablo Alan <pabloxt14@gmail.com>
2024-01-11 18:18:39 -0300

    refactor: ex107

--- execicios/ex107/main.py
+++ execicios/ex107/main.py
@@ -15,6 +15,6 @@ print('-' * line_length)
 print(f'* Metade de R${price:.2f}: {'\033[1;33m'}R${half(price):.2f}{'\033[m'}')
 print(f'* Dobro de R${price:.2f}: {'\033[1;33m'}R${double(price):.2f}{'\033[m'}')
 print(f'* Aumentando {increase_percentage}% (em R${price:.2f}) temos: {'\033[1;33m'}R${increase(price, increase_percentage):.2f}{'\033[m'}')
-print(f'* Diminuindo {decrease_percentage}% (em R${price:.2f}) temos: {'\033[1;33m'}R${decrease(price, increase_percentage):.2f}{'\033[m'}')
+print(f'* Diminuindo {decrease_percentage}% (em R${price:.2f}) temos: {'\033[1;33m'}R${decrease(price, decrease_percentage):.2f}{'\033[m'}')
 
 print('-' * line_length)
